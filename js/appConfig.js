3edce3ba6c2d6392dced7b7fe8744bdbb4063ab5
Éric Lemoine <eric.lemoine@oslandia.com>
2019-01-16 14:28:02 +0100

    Customize application

--- js/appConfig.js
+++ js/appConfig.js
@@ -12,14 +12,7 @@ const EditingInterface = require('./EditingInterface');
 const CoordinatesUtils = require('../qwc2/MapStore2Components/utils/CoordinatesUtils');
 const renderHelp = require('./Help');
 
-Proj4js.defs("EPSG:21781", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=600000 +y_0=200000 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs");
-Proj4js.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
-Proj4js.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
-
 CoordinatesUtils.setCrsLabels({
-    "EPSG:21781": "CH1903 / LV03",
-    "EPSG:2056": "CH1903+ / LV95",
-    "EPSG:25832": "ETRS89 / UTM 32N"
 });
 
 module.exports = {
@@ -30,7 +23,7 @@ module.exports = {
     pluginsDef: {
         plugins: {
             MapPlugin: require('../qwc2/QWC2Components/plugins/Map')({
-                EditingSupport: require('../qwc2/QWC2Components/plugins/map/EditingSupport'),
+                //EditingSupport: require('../qwc2/QWC2Components/plugins/map/EditingSupport'),
                 MeasurementSupport: require('../qwc2/QWC2Components/plugins/map/MeasurementSupport'),
                 LocateSupport: require('../qwc2/QWC2Components/plugins/map/LocateSupport'),
                 OverviewSupport: require('../qwc2/QWC2Components/plugins/map/OverviewSupport'),
@@ -55,16 +48,16 @@ module.exports = {
             LayerTreePlugin: require('../qwc2/QWC2Components/plugins/LayerTree'),
             IdentifyPlugin: require('../qwc2/QWC2Components/plugins/Identify'),
             MapTipPlugin: require('../qwc2/QWC2Components/plugins/MapTip'),
-            SharePlugin: require('../qwc2/QWC2Components/plugins/Share'),
+            //SharePlugin: require('../qwc2/QWC2Components/plugins/Share'),
             MapCopyrightPlugin: require('../qwc2/QWC2Components/plugins/MapCopyright'),
-            PrintPlugin: require('../qwc2/QWC2Components/plugins/Print'),
+            //PrintPlugin: require('../qwc2/QWC2Components/plugins/Print'),
             HelpPlugin: require('../qwc2/QWC2Components/plugins/Help')(renderHelp),
-            DxfExportPlugin: require('../qwc2/QWC2Components/plugins/DxfExport'),
-            RasterExportPlugin: require('../qwc2/QWC2Components/plugins/RasterExport'),
+            //DxfExportPlugin: require('../qwc2/QWC2Components/plugins/DxfExport'),
+            //RasterExportPlugin: require('../qwc2/QWC2Components/plugins/RasterExport'),
             RedliningPlugin: require('../qwc2/QWC2Components/plugins/Redlining'),
-            EditingPlugin: require('../qwc2/QWC2Components/plugins/Editing')(EditingInterface),
-            MapComparePlugin: require('../qwc2/QWC2Components/plugins/MapCompare'),
-            HeightProfilePlugin: require('../qwc2/QWC2Components/plugins/HeightProfile'),
+            //EditingPlugin: require('../qwc2/QWC2Components/plugins/Editing')(EditingInterface),
+            //MapComparePlugin: require('../qwc2/QWC2Components/plugins/MapCompare'),
+            //HeightProfilePlugin: require('../qwc2/QWC2Components/plugins/HeightProfile'),
             MapInfoTooltipPlugin: require('../qwc2/QWC2Components/plugins/MapInfoTooltip'),
             IdentifyRegionPlugin: require('../qwc2/QWC2Components/plugins/IdentifyRegion')
         },
